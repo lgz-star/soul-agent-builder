78a01e0e92851017183b105ea3c2d8258bbb3c30
lgz-star <2537962214@qq.com>
2026-03-21 01:18:05 +0800

    chore: update vite.config for LAN access

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './',
+  server: {
+    host: '0.0.0.0', // 允许局域网访问
+    port: 5173,
+  },
   build: {
     outDir: 'dist',
   },
